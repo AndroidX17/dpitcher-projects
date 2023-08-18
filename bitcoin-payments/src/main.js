d1e70bb5578af79f96d06f91f1aacdd85c555563
David Pitcher <114188266+davidvpitcher@users.noreply.github.com>
2023-08-18 09:15:09 -0700

    Delete bitcoin-payments directory

--- bitcoin-payments/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { Buffer } from 'buffer';
-window.Buffer = Buffer; // Note: `window` won't be defined if you're running in a Node context
-
-createApp(App).mount('#app')
